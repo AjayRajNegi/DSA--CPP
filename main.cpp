b2e529f7c7c1eb8df4f27b3723e28d30f5e62f14
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-10 13:19:38 +0530

    Unordered map

--- main.cpp
+++ main.cpp
@@ -3,17 +3,7 @@ using namespace std;
 
 int main(){
     int n;
-    cin >> n;
-    int arr[n];
-    map<int, int> mpp;
-    for(int i =0; i<n;i++){
-        cin >> arr[i];
-        mpp[arr[i]]++;
-    }
 
-
-    int q;
-    cin >> q;
     while(q--){
         int number;
         cin >> number;
