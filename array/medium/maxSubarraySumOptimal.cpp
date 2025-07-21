564b3be115664c1f2b1c780e357d4f5767b663de
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-21 09:36:32 +0530

    maxSubarraySumOptimal

--- array/medium/maxSubarraySumOptimal.cpp
+++ array/medium/maxSubarraySumOptimal.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSubarraySum(int arr[], int n) {
-    int maxi = INT_MIN; 
-int sum =0;
-    for(int i=0;i<n;i++){
+int maxSubarraySum(int arr[], int n)
+{
+    int maxi = INT_MIN;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
         sum += arr[i];
 
-        if(sum>maxi){
-            maxi=sum;
+        if (sum > maxi)
+        {
+            maxi = sum;
         }
 
-        if(sum<0){
-            sum=0;
+        if (sum < 0)
+        {
+            sum = 0;
         }
-
     }
 
     return maxi;
@@ -22,7 +25,7 @@ int sum =0;
 
 int main()
 {
-    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
     int maxSum = maxSubarraySum(arr, n);
     cout << "The maximum subarray sum is: " << maxSum << endl;
