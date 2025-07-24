e63f7b02b0f79df758163bf47bd094609412fabb
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-24 10:24:48 +0530

    matrixRotate

--- main.cpp
+++ main.cpp
@@ -1,60 +1,30 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
-
-vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
-{
-
-  int row[n] = {0}; // row array
-  int col[m] = {0}; // col array
-
-  // Traverse the matrix:
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      if (matrix[i][j] == 0)
-      {
-        // mark ith index of row wih 1:
-        row[i] = 1;
-
-        // mark jth index of col wih 1:
-        col[j] = 1;
-      }
+void rotate(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    //transposing the matrix
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
     }
-  }
-
-  // Finally, mark all (i, j) as 0
-  // if row[i] or col[j] is marked with 1.
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      if (row[i] || col[j])
-      {
-        matrix[i][j] = 0;
-      }
     }
-  }
-
-  return matrix;
+    //reversing each row of the matrix
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
 }
 
-int main()
-{
-  vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-  int n = matrix.size();
-  int m = matrix[0].size();
-  vector<vector<int>> ans = zeroMatrix(matrix, n, m);
-
-  cout << "The Final matrix is: n";
-  cout << endl;
-  for (auto it : ans)
-  {
-    for (auto ele : it)
-    {
-      cout << ele << " ";
+int main() {
+    vector < vector < int >> arr;
+    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotate(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+        cout << arr[i][j] << " ";
     }
     cout << " ";
-  }
-  return 0;
+    }
+
 }
