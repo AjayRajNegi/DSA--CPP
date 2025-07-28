af371248c87815726f345810f78256565b007456
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-28 09:37:32 +0530

    pascal's triangle

--- array/hard/pascal.cpp
+++ array/hard/pascal.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> generateRow(int row) {
+vector<int> generateRow(int row)
+{
     long long ans = 1;
     vector<int> ansRow;
-    ansRow.push_back(1); //inserting the 1st element
+    ansRow.push_back(1); // inserting the 1st element
 
-    //calculate the rest of the elements:
-    for (int col = 1; col < row; col++) {
+    // calculate the rest of the elements:
+    for (int col = 1; col < row; col++)
+    {
         ans = ans * (row - col);
         ans = ans / col;
         ansRow.push_back(ans);
@@ -15,11 +17,13 @@ vector<int> generateRow(int row) {
     return ansRow;
 }
 
-vector<vector<int>> pascalTriangle(int n) {
+vector<vector<int>> pascalTriangle(int n)
+{
     vector<vector<int>> ans;
 
-    //store the entire pascal's triangle:
-    for (int row = 1; row <= n; row++) {
+    // store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++)
+    {
         ans.push_back(generateRow(row));
     }
     return ans;
@@ -29,8 +33,10 @@ int main()
 {
     int n = 5;
     vector<vector<int>> ans = pascalTriangle(n);
-    for (auto it : ans) {
-        for (auto ele : it) {
+    for (auto it : ans)
+    {
+        for (auto ele : it)
+        {
             cout << ele << " ";
         }
         cout << endl;
