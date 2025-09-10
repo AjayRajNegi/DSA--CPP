f0f035f19e4fb08fab10bd4d7a9a0e1d5f3f63a4
Ajay <ajayrajnegi111@gmail.com>
2025-09-10 11:50:22 +0530

    segregateOddEven(Brute Force)

--- LL/DeleteMIddleNode.cpp
+++ LL/DeleteMIddleNode.cpp
@@ -3,58 +3,58 @@ using namespace std;
 
 struct ListNode
 {
-    int val;
-    ListNode *next;
-    ListNode(int val) : val(val), next(nullptr) {}
+	int val;
+	ListNode *next;
+	ListNode(int val) : val(val), next(nullptr) {}
 };
 
 ListNode *deleteMiddle(ListNode *head)
 {
-    if (!head || !head->next)
-        return nullptr;
+	if (!head || !head->next)
+		return nullptr;
 
-    int n = 0;
-    ListNode *temp = head;
-    while (temp)
-    {
-        n++;
-        temp = temp->next;
-    }
+	int n = 0;
+	ListNode *temp = head;
+	while (temp)
+	{
+		n++;
+		temp = temp->next;
+	}
 
-    if (n == 1)
-        return nullptr;
-    if (n == 2)
-    {
-        head->next = nullptr;
-        return head;
-    }
-    else
-    {
+	if (n == 1)
+		return nullptr;
+	if (n == 2)
+	{
+		head->next = nullptr;
+		return head;
+	}
+	else
+	{
 
-        temp = head;
-        for (int i = 0; i < (n / 2) - 1; i++)
-        {
-            temp = temp->next;
-        }
+		temp = head;
+		for (int i = 0; i < (n / 2) - 1; i++)
+		{
+			temp = temp->next;
+		}
 
-        ListNode *current = temp->next;
-        temp->next = temp->next->next;
-        delete current;
-    }
+		ListNode *current = temp->next;
+		temp->next = temp->next->next;
+		delete current;
+	}
 
-    return head;
+	return head;
 }
 int main()
 {
-    ListNode *head = new ListNode(1);
-    head->next = new ListNode(4);
-    head->next->next = new ListNode(6);
-    head->next->next->next = new ListNode(12);
-    head->next->next->next->next = new ListNode(987);
-    head->next->next->next->next->next = new ListNode(343);
+	ListNode *head = new ListNode(1);
+	head->next = new ListNode(4);
+	head->next->next = new ListNode(6);
+	head->next->next->next = new ListNode(12);
+	head->next->next->next->next = new ListNode(987);
+	head->next->next->next->next->next = new ListNode(343);
 
-    // cout << (deleteMiddle(head) ? "Cycle detected" : "No cycle") << endl;
-    cout << deleteMiddle(head);
+	// cout << (deleteMiddle(head) ? "Cycle detected" : "No cycle") << endl;
+	cout << deleteMiddle(head);
 
-    return 0;
+	return 0;
 }
